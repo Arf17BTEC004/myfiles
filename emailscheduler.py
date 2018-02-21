b35efa7390d387b5d182670b6ec26e2e0bca8e24
Arf17BTEC004 <35484231+Arf17BTEC004@users.noreply.github.com>
2018-02-21 11:37:18 +0530

    Add files via upload

--- emailscheduler.py
+++ emailscheduler.py
@@ -1,2 +1,11 @@
-a=9%(4//2)
-print(a)
+
+
+import smtplib
+ 
+s= smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login("arfath.17ec@cmr.edu.in", "arfath@cmru")
+message = "heyyyyyyyy"
+s.sendmail("arfath.17ec@cmr.edu.in", "arfathbaig355@gmail.com", message)
+s.quit()
+print("success")
